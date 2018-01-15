1a89d92b0acf2392106c4d2b20811665ddfd9c5d
Bruna Meneguzzi <33071859+BrunaMeneguzzi@users.noreply.github.com>
2018-01-15 16:28:25 -0200

    Rename MediaWiki/Common.js/WatchlistNotice.js to MediaWiki/Common.js_WatchlistNotice.js

---
+++ MediaWiki/Common.js_WatchlistNotice.js
@@ -0,0 +1,25 @@
+/* Any JavaScript here will be loaded for all users on every page load. */
+/*
+ *  Description: Hide the watchlist message for one week.
+ *  Maintainers: [[w:User:Ruud Koot|Ruud Koot]]
+ *  Updated by:  [[User:Darklama|darklama]]
+ */
+ 
+function addDismissButton() {
+	var $watchlistMessage = $('#watchlist-message');
+ 
+	if ( !$watchlistMessage.length ) return;
+	if ( $.cookie('hidewatchlistmessage') === 'yes' ) {
+		$watchlistMessage.hide();
+		return;
+	}
+ 
+	$('<span>[<a id="dismissButton" title="Hide this message for one week">dismiss</a>]</span>')
+		.appendTo( $watchlistMessage )
+		.click( function() {
+			$.cookie('hidewatchlistmessage', 'yes', { 'expires': 7, 'path': '/' });
+			$watchlistMessage.hide();
+		});
+}
+ 
+if ( mw.config.get('wgCanonicalSpecialPageName') === 'Watchlist') $(document).ready( addDismissButton );
